8f25a659cbc64f9bce14bb03cbe04b4de5f98631
orion <crissorion@gmail.com>
2018-07-01 16:24:08 +0300

    - the most base thumbnail switch logic

--- src/components/JsxCarousel/JsxCarousel.js
+++ src/components/JsxCarousel/JsxCarousel.js
@@ -1,25 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './JsxCarousel.css';
 
-const Slide = slide =>
-    <li>
-        <div style={{ background: `url(${slide.url})`, width: '600px', height: '400px' }}/>
-    </li>;
+const Slide = slide => (
+    <div
+        className="JsxCarousel__Slide"
+        style={{ background: `url(${slide.url})` }}/>);
 
-export default ({ slides }) =>
-    <div className="JsxCarousel">
-        <div className="JsxCarousel__carousel">
-            <div className="JsxCarousel__active-image">
-                <div style={{ background: `url(${slides[0].url})`, width: '600px', height: '400px' }}/>
+const Thumbnail = ({ slide, isActive, onSelect }) => (
+    <li className={'JsxCarousel__thumbnails__thumbnail' + (isActive ? ' active' : '')}>
+        <div style={{ background: `url(${slide.thumbnail})` }}
+             onClick={onSelect}/>
+    </li>
+);
+
+const Thumbnails = ({ slides, active, onSelect }) => (
+    <ul>
+        {
+            slides.map((it, ix) =>
+                <Thumbnail key={it.thumbnail} onSelect={() => onSelect(ix)} isActive={ix === active} slide={it}/>)
+        }
+    </ul>);
+
+class JsxCarousel extends Component {
+    state = {
+        current: 0
+    };
+
+    onSlideSelect = ix => this.setState({ current: ix });
+
+    render() {
+        const { slides } = this.props;
+
+        return (
+            <div className="JsxCarousel">
+                <div className="JsxCarousel__wrapper">
+                    <div className="JsxCarousel__slider">
+                        <div className="JsxCarousel__slider__active">
+                            <Slide {...slides[this.state.current]}/>
+                        </div>
+                        <div className="JsxCarousel__slider__slides">
+                            {slides.map(it => <Slide key={it.url} {...it}/>)}
+                        </div>
+                    </div>
+                    <div className="JsxCarousel">DESCRIPTION</div>
+                </div>
+                <div className="JsxCarousel__thumbnails">
+                    <Thumbnails slides={slides} active={this.state.current} onSelect={this.onSlideSelect}/>
+                </div>
             </div>
-            <ul className="JsxCarousel__list">
-                {slides.map(it => <Slide key={it.url} {...it}/>)}
-            </ul>
-            <div className="JsxCarousel">DESCRIPTION</div>
-        </div>
-        <div className="JsxCarousel__thumbnails">
-            <ul>
-                {slides.map(it => <li key={it.thumbnail}>{it.title}</li>)}
-            </ul>
-        </div>
-    </div>;
+        );
+    }
+
+}
+
+export default JsxCarousel;
