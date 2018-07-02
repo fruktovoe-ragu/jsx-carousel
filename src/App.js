a5d692bb39cfebfc59f0cc249473f880980e2aee
orion <crissorion@gmail.com>
2018-07-02 11:30:56 +0300

    - description update

--- src/App.js
+++ src/App.js
@@ -6,22 +6,46 @@ const slides = [
     {
         url: 'https://www.popsci.com/sites/popsci.com/files/styles/655_1x_/public/images/2018/03/senna.jpg?itok=4dsvu7gl&fc=50,50',
         thumbnail: 'https://www.popsci.com/sites/popsci.com/files/styles/655_1x_/public/images/2018/03/senna.jpg?itok=4dsvu7gl&fc=50,50',
-        title: 'car 1',
+        description: {
+            leftTitle: 'Team',
+            team: 'car 1',
+            rightTitle: 'App',
+            appName: 'app 1',
+            descriptionText: 'app... app.. app'
+        },
     },
     {
         url: 'https://apollo2.dl.playstation.net/cdn/EP0700/CUSA04932_00/FREE_CONTENTm3RVj9ffBRTN5fAuBOr0/PREVIEW_SCREENSHOT1_488862.jpg',
         thumbnail: 'https://apollo2.dl.playstation.net/cdn/EP0700/CUSA04932_00/FREE_CONTENTm3RVj9ffBRTN5fAuBOr0/PREVIEW_SCREENSHOT1_488862.jpg',
-        title: 'car 2',
+        description: {
+            leftTitle: 'Team',
+            team: 'car 2',
+            rightTitle: 'App',
+            appName: 'app 2',
+            descriptionText: 'app... app.. app'
+        },
     },
     {
         url: 'https://apollo2.dl.playstation.net/cdn/EP0700/CUSA04932_00/FREE_CONTENTM8KBV6c0da80qa0xNWEp/PREVIEW_SCREENSHOT3_488862.jpg',
         thumbnail: 'https://apollo2.dl.playstation.net/cdn/EP0700/CUSA04932_00/FREE_CONTENTM8KBV6c0da80qa0xNWEp/PREVIEW_SCREENSHOT3_488862.jpg',
-        title: 'car 3',
+        description: {
+            leftTitle: 'Team',
+            team: 'car 3',
+            rightTitle: 'App',
+            appName: 'app 3',
+            descriptionText: 'app... app.. app'
+        },
     },
     {
         url: 'https://apollo2.dl.playstation.net/cdn/EP0700/CUSA04932_00/FREE_CONTENTWSsDL20KF3hEakHcJ3D5/PREVIEW_SCREENSHOT6_488862.jpg',
         thumbnail: 'https://apollo2.dl.playstation.net/cdn/EP0700/CUSA04932_00/FREE_CONTENTWSsDL20KF3hEakHcJ3D5/PREVIEW_SCREENSHOT6_488862.jpg',
-        title: 'car 4',
+        description: {
+            leftTitle: 'Team',
+            team: 'car 4',
+            rightTitle: 'App',
+            appName: 'app 4',
+            descriptionText: 'app... app.. app'
+        },
     }
 ];
 
--- src/components/JsxCarousel/Description.js
+++ src/components/JsxCarousel/Description.js
@@ -1,3 +1,15 @@
 import React from 'react';
 
-export default ({ slide }) => <div className="JsxCarousel">{slide.title}</div>;
+export default ({ slide }) => (
+    <div className="JsxCarousel__description">
+        <div>
+            <div>{slide.description.leftTitle}</div>
+            <div>{slide.description.team}</div>
+        </div>
+        <div>
+            <div>{slide.description.rightTitle}</div>
+            <div>{slide.description.appName}</div>
+            <div>{slide.description.descriptionText}</div>
+        </div>
+    </div>
+);
