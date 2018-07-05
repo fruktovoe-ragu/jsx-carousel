178420799d3955fe965c68aecb9ebde2ea6e188d
Alexandra <fruktovoe.ragu@gmail.com>
2018-07-05 10:07:50 +0300

    add classes in Description.jsx

--- src/components/JsxCarousel/Description.js
+++ src/components/JsxCarousel/Description.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
 export default ({ slide }) => (
-    <div className="JsxCarousel__description">
-        <div>
-            <div>{slide.description.leftTitle}</div>
-            <div>{slide.description.team}</div>
-        </div>
-        <div>
-            <div>{slide.description.rightTitle}</div>
-            <div>{slide.description.appName}</div>
-            <div>{slide.description.descriptionText}</div>
-        </div>
+   <div className="JsxCarousel__description grid">
+    <div className="grid-3-5">
+      <div className="JsxCarousel__description-title">{slide.description.leftTitle}</div>
+      <div className="JsxCarousel__description-name">{slide.description.team}</div>
     </div>
+    <div className="grid-2-5 JsxCarousel__description-app">
+      <div className="JsxCarousel__description-title">{slide.description.rightTitle}</div>
+      <div className="JsxCarousel__description-name">{slide.description.appName}</div>
+      <div className="JsxCarousel__description-desc">{slide.description.descriptionText}</div>
+    </div>
+  </div>
 );
