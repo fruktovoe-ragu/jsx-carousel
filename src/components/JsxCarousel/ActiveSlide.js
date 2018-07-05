1baff5fd62857c56f4fc5b2185c4c9dafb4b9d99
Alexandra <fruktovoe.ragu@gmail.com>
2018-07-05 10:06:33 +0300

    add class to ActiveSlide.jsx

--- src/components/JsxCarousel/ActiveSlide.js
+++ src/components/JsxCarousel/ActiveSlide.js
@@ -4,7 +4,7 @@ export default ({ slide, transform }) => {
     transform = Math.max(transform, 0);
 
     return (
-        <div className="JsxCarousel__slider__active"
+        <div className="JsxCarousel__slider__active JsxCarousel__slide-container"
              style={{ transform: `translateX(${transform}px)` }}
         >{slide.el}</div>
     );
